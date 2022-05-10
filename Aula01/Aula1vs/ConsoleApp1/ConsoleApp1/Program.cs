092d9c0df29f23bf16c2e66c9e40475650d0927f
Cleidenascimento <cleidenascimentto@hotmail.com>
2022-05-10 18:32:48 -0300

    Primeiro Commit Aula 01 Dotnet

---
+++ Aula01/Aula1vs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Cleide!");
+int a = 10;
+int b = 10;
+Console.WriteLine(a + b);  
+int c = a + b;
+Console.WriteLine(c);
+Console.WriteLine("outra coisa");
+
+
---
+++ Aula01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 10;
+int b = 20;
+int c = a + b;

850ca6aa15b608d46aa71dc75d086f4fc08d2dc1
Cleidenascimento <cleidenascimentto@hotmail.com>
2022-06-07 21:23:54 -0300

    Atualizaçoes aula 17 05

---
+++ Aula04-17-05-22/ExercicioMultiplicacaoVS/ExercicioMultiplicacaoVS/ExercicioMultiplicacaoVS/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite um numero");
+var numeroAstr = Console.ReadLine();
+Console.WriteLine("Digite um numero");
+var numeroBstr = Console.ReadLine();
+
+var numeroA = Convert.ToInt32(numeroAstr);
+var numeroB = Convert.ToInt32(numeroBstr);
+
+int multiplicacao = 0;
+for (int i = 0; 1 < numeroB; i++)
+{
+    multiplicacao += numeroA;
+}
+Console.WriteLine(multiplicacao);
